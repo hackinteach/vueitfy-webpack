cf8dc472dbf2a0b5f14c91e6970b06fd944e4032
Nuttapat Koonarangsri <webmaster@hackinteach.com>
2019-02-26 23:34:46 +0700

    add vuerouter

--- src/main.js
+++ src/main.js
@@ -3,13 +3,17 @@ import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css'; // Ensure you are using css-loader
 import '@fortawesome/fontawesome-free/css/all.css'; // Ensure you are using css-loader
 import App from './App.vue';
+import router from './router';
 
 Vue.use(Vuetify, {
   iconfont: 'fa',
 });
 
+Vue.mixin({});
+
 Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app');
